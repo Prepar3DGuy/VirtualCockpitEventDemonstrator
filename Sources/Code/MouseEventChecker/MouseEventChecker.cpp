98cf9d3e01ba0c60793504467979cc2e744efb95
Dima Ivanov <prepar3dguy@gmail.com>
2020-11-11 06:14:07 +0300

    Add SimConnect client to receive MouseRect EventID
    Using SimConnect_MapClientEventToSimEvent for Events with ID x110044,
    receive events, but dwData always 0. So this variant also doesn't can be
    used to distinguish LMB with RMB from single EventID.

--- Sources/Code/MouseEventChecker/MouseEventChecker.cpp
+++ Sources/Code/MouseEventChecker/MouseEventChecker.cpp
@@ -3,6 +3,7 @@
 #include <MSFS/MSFS_Render.h>
 #include <MSFS/Render/nanovg.h>
 #include <MSFS/Legacy/gauges.h>
+#include <SimConnect.h>
 
 #include <map>
 #include <deque>
@@ -21,16 +22,20 @@ const int cMaxLogLength = 25;
 std::deque<std::string> g_EventLog;
 char g_OutputStr[1000] = "";
 
+HRESULT hr;
+HANDLE hSimConnect = NULL;
+void CALLBACK SimConnectDispatchProc(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext);
+
 void MyEventHandler(ID32 event, UINT32 evdata, PVOID userdata)
 {
 	std::ostringstream strMsg;
 	switch (event)
 	{
 	case GREEN_BUTTON_ACTION_EVENTID:
-		strMsg << "T: " << g_Time << " GREEN BUTTON " << "data: " << evdata; // Why always evdata == MOUSE_NONE ???
+		strMsg << "T: " << g_Time << " EventHandler GREEN BUTTON " << "data: " << evdata; // Why always evdata == MOUSE_NONE ???
 		break;
 	case RED_BUTTON_ACTION_EVENTID:
-		strMsg << "T: " << g_Time << " RED BUTTON " << "data: " << evdata; // Why always evdata == MOUSE_NONE ???
+		strMsg << "T: " << g_Time << " EventHandler RED BUTTON " << "data: " << evdata; // Why always evdata == MOUSE_NONE ???
 		break;
 	default:
 		break;
@@ -49,6 +54,11 @@ extern "C"
 		{
 			sGaugeInstallData* p_install_data = (sGaugeInstallData*)pData;
 			register_key_event_handler((GAUGE_KEY_EVENT_HANDLER)MyEventHandler, NULL);
+			if (FAILED(SimConnect_Open(&hSimConnect, "MouseEventDisplay", NULL, 0, 0, 0)))
+			{
+				fprintf(stderr, "SimConnect_Open error.\n");
+				return false;
+			}
 			return true;
 		}
 		case PANEL_SERVICE_POST_INSTALL:
@@ -74,6 +84,13 @@ extern "C"
 		case PANEL_SERVICE_POST_INITIALIZE: break;
 		case PANEL_SERVICE_PRE_UPDATE:
 		{
+			hr = SimConnect_CallDispatch(hSimConnect, SimConnectDispatchProc, NULL);
+			if (FAILED(hr))
+			{
+				fprintf(stderr, "Error on call to SimConnect_CallDispatch.");
+				return false;
+			}
+
 			while (g_EventLog.size() > cMaxLogLength)
 			{
 				g_EventLog.pop_front();
@@ -118,6 +135,7 @@ extern "C"
 			NVGcontext* nvgctx = g_GaugeNVGcontext[ctx];
 			nvgDeleteInternal(nvgctx);
 			g_GaugeNVGcontext.erase(ctx);
+			SimConnect_Close(hSimConnect);
 			unregister_key_event_handler((GAUGE_KEY_EVENT_HANDLER)MyEventHandler, NULL);
 			return true;
 		}
@@ -184,5 +202,100 @@ extern "C"
 		strMsg << " x: " << fX << " y: " << fY;
 		g_EventLog.push_back(strMsg.str());
 		
+	}
+}
+
+std::map<SIMCONNECT_RECV_ID, std::string> cSimConnect_Recv_ID_description = {
+	{SIMCONNECT_RECV_ID_NULL, "SIMCONNECT_RECV_ID_NULL"},
+	{SIMCONNECT_RECV_ID_EXCEPTION, "SIMCONNECT_RECV_ID_EXCEPTION"},
+	{SIMCONNECT_RECV_ID_OPEN, "SIMCONNECT_RECV_ID_OPEN"},
+	{SIMCONNECT_RECV_ID_QUIT, "SIMCONNECT_RECV_ID_QUIT"},
+	{SIMCONNECT_RECV_ID_EVENT, "SIMCONNECT_RECV_ID_EVENT"},
+	{SIMCONNECT_RECV_ID_EVENT_OBJECT_ADDREMOVE, "SIMCONNECT_RECV_ID_EVENT_OBJECT_ADDREMOVE"},
+	{SIMCONNECT_RECV_ID_EVENT_FILENAME, "SIMCONNECT_RECV_ID_EVENT_FILENAME"},
+	{SIMCONNECT_RECV_ID_EVENT_FRAME, "SIMCONNECT_RECV_ID_EVENT_FRAME"},
+	{SIMCONNECT_RECV_ID_SIMOBJECT_DATA, "SIMCONNECT_RECV_ID_SIMOBJECT_DATA"},
+	{SIMCONNECT_RECV_ID_SIMOBJECT_DATA_BYTYPE, "SIMCONNECT_RECV_ID_SIMOBJECT_DATA_BYTYPE"},
+	{SIMCONNECT_RECV_ID_WEATHER_OBSERVATION, "SIMCONNECT_RECV_ID_WEATHER_OBSERVATION"},
+	{SIMCONNECT_RECV_ID_CLOUD_STATE, "SIMCONNECT_RECV_ID_CLOUD_STATE"},
+	{SIMCONNECT_RECV_ID_ASSIGNED_OBJECT_ID, "SIMCONNECT_RECV_ID_ASSIGNED_OBJECT_ID"},
+	{SIMCONNECT_RECV_ID_RESERVED_KEY, "SIMCONNECT_RECV_ID_RESERVED_KEY"},
+	{SIMCONNECT_RECV_ID_CUSTOM_ACTION, "SIMCONNECT_RECV_ID_CUSTOM_ACTION"},
+	{SIMCONNECT_RECV_ID_SYSTEM_STATE, "SIMCONNECT_RECV_ID_SYSTEM_STATE"},
+	{SIMCONNECT_RECV_ID_CLIENT_DATA, "SIMCONNECT_RECV_ID_CLIENT_DATA"},
+	{SIMCONNECT_RECV_ID_EVENT_WEATHER_MODE, "SIMCONNECT_RECV_ID_EVENT_WEATHER_MODE"},
+	{SIMCONNECT_RECV_ID_AIRPORT_LIST, "SIMCONNECT_RECV_ID_AIRPORT_LIST"},
+	{SIMCONNECT_RECV_ID_VOR_LIST, "SIMCONNECT_RECV_ID_VOR_LIST"},
+	{SIMCONNECT_RECV_ID_NDB_LIST, "SIMCONNECT_RECV_ID_NDB_LIST"},
+	{SIMCONNECT_RECV_ID_WAYPOINT_LIST, "SIMCONNECT_RECV_ID_WAYPOINT_LIST"},
+	{SIMCONNECT_RECV_ID_EVENT_MULTIPLAYER_SERVER_STARTED, "SIMCONNECT_RECV_ID_EVENT_MULTIPLAYER_SERVER_STARTED"},
+	{SIMCONNECT_RECV_ID_EVENT_MULTIPLAYER_CLIENT_STARTED, "SIMCONNECT_RECV_ID_EVENT_MULTIPLAYER_CLIENT_STARTED"},
+	{SIMCONNECT_RECV_ID_EVENT_MULTIPLAYER_SESSION_ENDED, "SIMCONNECT_RECV_ID_EVENT_MULTIPLAYER_SESSION_ENDED"},
+	{SIMCONNECT_RECV_ID_EVENT_RACE_END, "SIMCONNECT_RECV_ID_EVENT_RACE_END"},
+	{SIMCONNECT_RECV_ID_EVENT_RACE_LAP, "SIMCONNECT_RECV_ID_EVENT_RACE_LAP"}
+};
+
+enum NOTIFICATION_GROUP_ID
+{
+	GROUP0,
+};
+
+enum CLIENT_EVENT_ID
+{
+	GREEN_BUTTON_EVENT_ID,
+};
+
+void CALLBACK SimConnectDispatchProc(SIMCONNECT_RECV* pData, DWORD cbData, void* pContext)
+{
+	switch (pData->dwID)
+	{
+	case SIMCONNECT_RECV_ID_OPEN:
+	{
+		SIMCONNECT_RECV_OPEN* pRecvOpen = (SIMCONNECT_RECV_OPEN*)pData;
+		
+		if (FAILED(SimConnect_MapClientEventToSimEvent(hSimConnect, GREEN_BUTTON_EVENT_ID, "#0x11044")))
+		{
+			fprintf(stderr, "Unable to Map Event with ID #0x11044\n");
+			break;
+		}
+		SimConnect_AddClientEventToNotificationGroup(hSimConnect, GROUP0, GREEN_BUTTON_EVENT_ID);
+		SimConnect_SetNotificationGroupPriority(hSimConnect, GROUP0, SIMCONNECT_GROUP_PRIORITY_HIGHEST);
+
+		break;
+	}
+	case SIMCONNECT_RECV_ID_EVENT:	// ѕолучена событие
+	{
+		SIMCONNECT_RECV_EVENT* pRecvEvent = (SIMCONNECT_RECV_EVENT*)pData;
+		switch (pRecvEvent->uGroupID)
+		{
+		case GROUP0:
+		{
+			switch (pRecvEvent->uEventID)
+			{
+			case GREEN_BUTTON_EVENT_ID:
+			{
+				std::ostringstream strMsg;
+				strMsg << "T: " << g_Time << " SimConnect Green Button Event " << pRecvEvent->dwData;
+				g_EventLog.push_back(strMsg.str());
+				break;
+			}
+			}
+			break;
+		}
+		default:
+		{
+			fprintf(stderr, "Receive Unknown Event. Group %d ID %d\n", pRecvEvent->uGroupID, pRecvEvent->uEventID);
+		}
+		}
+		break;
+	}
+	case SIMCONNECT_RECV_ID_NULL: // ничего полезного не получили
+	{
+		break;
+	}
+	default:
+	{
+		fprintf(stderr, "Unknown SimConnect message: %s \n", cSimConnect_Recv_ID_description[(SIMCONNECT_RECV_ID)pData->dwID].c_str());
+	}
 	}
 }
